e013b290efad1c69dc865a311f7114d4457f8101
TkTnX <timur20-09@mail.ru>
2024-09-15 19:41:29 +0500

    секция со слайдером

---
+++ script.js
@@ -0,0 +1,14 @@
+const rewardsSwiper = new Swiper(".rewards__swiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
