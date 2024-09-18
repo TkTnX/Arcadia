c0087cbd35ab7338fb8d946fd29f84f077531993
TkTnX <timur20-09@mail.ru>
2024-09-18 14:55:45 +0500

    разрешение 768 готово, бургер меню

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
+// Burger Menu
+
+const burgerMenuButton = document.querySelector("#burgerButton");
+const burgerMenu = document.querySelector("#burgerMenu");
+
+burgerMenuButton.addEventListener("click", () => {
+  burgerMenu.classList.toggle("burger-menu__active");
+  burgerMenuButton.classList.toggle("burger-button__active");
+});
+
+// Slider
 const rewardsSwiper = new Swiper(".rewards__swiper", {
-  slidesPerView: 3,
-  spaceBetween: 30,
+  slidesPerView: 2,
+  spaceBetween: 10,
 
   pagination: {
     el: ".swiper-pagination",
@@ -11,4 +22,11 @@ const rewardsSwiper = new Swiper(".rewards__swiper", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+
+  breakpoints: {
+    1410: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    }
+  }
 });
